80ac743c3750a10425e265aa0271aea8f0a84b52
Camilo <juan.gonzalez.bernal@correounivalle.edu.co>
2025-11-08 18:12:10 -0500

    Pantalla inicial añadida; boton de reglas y jugar pendientes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example._50zo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example._50zo to javafx.fxml;
+    opens com.example._50zo.controller to javafx.fxml;
     exports com.example._50zo;
+    exports  com.example._50zo.controller to javafx.fxml;
 }
